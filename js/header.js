a9a334a2a111102fd901e57684b964c5ddbf62ba
Corentin Miserque <miserquecorentin@gmail.com>
2022-12-06 14:03:33 +0100

    modif

--- js/header.js
+++ js/header.js
@@ -1,11 +1,9 @@
 const menu = document.querySelector(".menu");
-const nav = document.querySelector("nav");
+const nav = document.querySelector(".nav")
 
 
 menu.addEventListener("click", () => {
-    nav.classlist.add('open')
-});
-
-
+    menu.classList.toggle('active');
+})
 
 
