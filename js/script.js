0787a9fa7947452f36dbeb7f869296daf627152b
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2022-12-07 15:10:47 +0100

    responsive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector(".list_button");
+var last_button="";
 menu.addEventListener("click",(e) =>{
 if(e.target.classList.contains('button')){
     menu.style.setProperty('--underline-width', `${e.target.offsetWidth}px`);
+    last_button=e.target
     menu.style.setProperty('--underline-offset-x', `${e.target.offsetLeft}px`)
-    menu.style.setProperty('--underline-offset-y', `${e.target.offsetTop}px`);
 }})
+
+
+
+addEventListener("resize", (event) => {
+    menu.style.setProperty('--underline-offset-x', `${last_button.offsetLeft}px`)
+    menu.style.setProperty('--underline-width', `${last_button.offsetWidth}px`);
+});
